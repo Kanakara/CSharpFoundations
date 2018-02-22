b0fe5c4109e712205cb73f2be23964deee589077
Reginald Beason <beason.reginald@gmail.com>
2018-02-22 13:59:03 -0800

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFoundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Write Your Name...");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+        }
+    }
+}
